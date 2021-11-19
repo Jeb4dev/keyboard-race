2c9a53ac84c546e4838cde58fdc424dc6c7b155e
mihett05 <mihett05@yandex.ru>
2021-11-19 23:24:02 +0500

    Added react-router and prettier

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from '../pages/home';
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
