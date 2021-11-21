d2218e9b6847ce5700b832dbf61db3002c646f9b
JesperKauppinen <jesperkauppinen@gmail.com>
2021-11-21 19:05:05 +0200

    + title

--- app/models/words.py
+++ app/models/words.py
@@ -6,6 +6,7 @@ class Words(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     words = db.Column(db.String(1000), nullable=False)
+    words_title = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
-        return f"Words(id={self.id})"
+        return f"Words(id={self.id}, title={self.words_title})"
