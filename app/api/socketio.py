0afb0dc98a4267d8137583b4400b6f7b781e73dd
jespe <jesperkauppinen@gmail.com>
2021-11-25 06:35:28 +0200

    tested sockets, some fixes, some issues

--- app/api/socketio.py
+++ app/api/socketio.py
@@ -1,8 +1,8 @@
 from flask_socketio import emit, join_room, leave_room, close_room, SocketIO
 from flask_jwt_extended import jwt_required
+
 from app.api.jwt import get_current_user
 from app.models import Words
-
 from app.models.db import db
 
 """
@@ -13,6 +13,8 @@ socketio = SocketIO(logger=True, cors_allowed_origins="*", async_mode="eventlet"
 
 active_rooms = {}
 
+
+# TODO: error handling, expired token, wrong / missing data
 
 # Redirect users to landing page
 # Close / remove room
@@ -29,6 +31,7 @@ def remove_room(room_name):
 
 # Handle saving statistics to the database
 def handle_statistics(user, data):
+    # TODO: check if this works
     # Update statistics object with new averages etc.
     if data.wpm > user.statistics.best_wpm:
         user.statistics.best_wpm = data.wpm
@@ -77,12 +80,15 @@ def on_connect():
 # When user disconnects
 @socketio.on('disconnect')
 def on_disconnect():
-    user = get_current_user()
+    print("Unknown user disconnected!")
+    return True
+    # TODO: ISSUE: get_current_user() returns error as user is no more current... ?
+    user = get_current_user()  # This doesnt work
 
     # Check if user was race leader, if -> remove lobby
     if active_rooms[user.id]:
         remove_room(user.id)
-        active_rooms[user.id].clear()  # Does this work?
+        del active_rooms[user.id]  # Does this work?
     # Iterate trough dict, check if user id in roomname.usernames, if -> leave room
     for owner_id, room in active_rooms.items():
         if user.id in room["username"]:
@@ -102,10 +108,7 @@ def create_race(data):
 
     room_title = f"{user.username}'s room"
 
-    if data["room"]:
-        room_name = data["room"]
-    else:
-        room_name = user_id
+    room_name = user_id
 
     # Check if room exist
     if room_name not in active_rooms:
@@ -123,8 +126,16 @@ def create_race(data):
         "room_title": room_title
     }
 
+    # If the wordlist is empty, create one.
+    if len(Words.query.all()) < 1:
+        words_ = Words(words="""One login. All your devices. A family of products that respect your privacy.
+                        Join Firefox""", words_title="test", user_id=user.id)
+        db.session.add(words_)
+        db.session.commit()
+        print("Created new record in Words table")
+
     emit('cl_create_race', user_id)  # must call sv_join_race
-    print(f"{user.username} created {room_title}!")
+    print(f"{user.username} created {room_title} with id {user_id}!")
 
 
 # Join room
@@ -156,15 +167,25 @@ def leave_race(data):
 
     # announce room that user left, if this is even needed
     emit('cl_user_left_race', user_id, to=room_name)
+
     # Leave room
     leave_room(room_name)
+
+    # Remove user from room dict
+    active_rooms[room_name]["users"].remove(user.id)
+
+    # if user was creator of room, -> delete room
+    if active_rooms[user.id]:
+        remove_room(user.id)
+        del active_rooms[user.id]
+
     print(f"{user.username} left {data['room_title']}!")
 
 
 # Get list of active rooms
 @socketio.on('sv_get_active_rooms')
 @jwt_required()
-def list_active_races():
+def list_active_races(data):
     user = get_current_user()
 
     # return active_rooms dictionary, that contains active_room_id, user_id's, room_title
@@ -182,9 +203,10 @@ def start_race(data):
     room_title = active_rooms[room_name]["room_title"]
 
     # Send word list to clients
-    wordlist_title = ""
-    wordlist = ""
-    emit('cl_get_wordlist', wordlist_title, wordlist, to=room_name)
+    words = Words.query.first()
+    wordlist_title = words.words_title
+    wordlist = words.words
+    emit('cl_get_wordlist', (wordlist_title, wordlist), to=room_name)
 
     # Tell clients to start race
     emit('cl_start_race', data, to=room_name)
@@ -196,7 +218,7 @@ def start_race(data):
 @jwt_required()
 def get_progress(data):  # data should at least contain room name, user whose data it is and what is the progress level
     user = get_current_user()
-    words = Words.query.all()
+    words = Words.query.first()
 
     room_name = data["room"]
 
@@ -204,7 +226,7 @@ def get_progress(data):  # data should at least contain room name, user whose da
     wordlist = words.words
 
     # Send word list to clients
-    emit('cl_get_wordlist', words_title, wordlist, to=room_name)
+    emit('cl_get_wordlist', (words_title, wordlist), to=room_name)
 
     # Tell clients to start race
     emit('cl_get_progress', data, to=room_name)
@@ -216,6 +238,7 @@ def get_progress(data):  # data should at least contain room name, user whose da
 @socketio.on('sv_get_race_statistics')
 @jwt_required()
 def get_race_statistics(data):
+    # TODO: test if this socket work as intended, also if function handle_statistics() work.
     # data should at least contain: room name, wpm, epm, ranking,
     # total participants, accuracy, race time, words title, errors
 
